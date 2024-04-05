f8382a81380b4c4595214d26b78ba9abac66c058
ioana.girba <>
2024-04-05 10:02:23 +0300

    Start application

---
+++ GoogleCloneApp/App.tsx
@@ -0,0 +1,17 @@
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = (): React.JSX.Element => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+
+      <View>
+        <Text>Google clone</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
